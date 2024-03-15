740e1cac59637f76dbb4a585a5e74bae8aaa8cd1
joduckyong <dyddl7609@gmail.com>
2024-03-15 15:59:11 +0900

    수정

--- build.sh
+++ build.sh
@@ -1,13 +1,20 @@
+#!/bin/bash
+# build.sh, PROJECT_NAME 값은 변경하기
+
 REPOSITORY=/var/lib/jenkins/workspace
 echo "REPOSITORY = $REPOSITORY"
 
-PROJECT_NAME=Music
-echo "PROJECT_NAME = $PROJECT_NAME"
+if [ -n "$MUSIC" ]; then
+  PROJECT_NAME="$MUSIC"
+else
+  PROJECT_NAME=$(sudo basename "$(pwd)")
+fi
+echo PROJECT_NAME ::: $PROJECT_NAME
 
 cd $REPOSITORY/$PROJECT_NAME
 
 echo "sudo chmod +x $REPOSITORY/$PROJECT_NAME/gradlew"
 sudo chmod +x $REPOSITORY/$PROJECT_NAME/gradlew
 
-echo "sudo $REPOSITORY/$PROJECT_NAME/gradlew build"
-sudo $REPOSITORY/$PROJECT_NAME/gradlew build
+echo "sudo $REPOSITORY/$PROJECT_NAME/gradlew clean build"
+sudo $REPOSITORY/$PROJECT_NAME/gradlew clean build
